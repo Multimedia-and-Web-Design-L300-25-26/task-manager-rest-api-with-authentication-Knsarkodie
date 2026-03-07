f3b71fb8420e0ad2fe39b3161647f4bb20e07ec1
Knsarkodie <sarkcessacd.13@gmail.com>
2026-03-07 23:52:41 +0000

    Completed

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,3 +1,7 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+// Load .env.test if NODE_ENV is test, otherwise .env
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+dotenv.config({ path: envFile });
 
 process.env.NODE_ENV = 'test';
